396a54dad25590c15c8c5628910fefd8869adcb9
Nivethini.S <36626348+NivethiniS@users.noreply.github.com>
2018-03-15 17:09:29 +0530

    Create Own2.java

---
+++ Own2.java
@@ -0,0 +1,30 @@
+public class Own2
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Array Size : ");
+		int n = sc.nextInt();
+		int a[] = new int [n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					int temp = a[j];
+					a[j] = a[i];
+					a[i] = temp;
+				}
+			}
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(a[i]);
+		}
+	}
+}
